b0ba9f0af62dd0810a937044dc4481e9f8d21ba8
moskv08 <fabrizio_39@hotmail.com>
2024-11-20 08:01:25 +0100

    feat: Initial Onboarding screen setup

--- app/src/main/java/com/loc/newsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/loc/newsapp/ui/theme/Theme.kt
@@ -34,7 +34,8 @@ private val LightColorScheme = lightColorScheme(
 fun NewsAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    // dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
