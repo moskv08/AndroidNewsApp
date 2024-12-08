71ec6a8e7a446cf589470a809eca06c6e4d3d772
moskv08 <fabrizio_39@hotmail.com>
2024-12-08 13:11:05 +0100

    Define localusermanager interface and usecases

--- app/src/main/java/com/loc/newsapp/domain/usecases/ReadAppEntry.kt
+++ app/src/main/java/com/loc/newsapp/domain/usecases/ReadAppEntry.kt
@@ -1,11 +1,12 @@
 package com.loc.newsapp.domain.usecases
 
 import com.loc.newsapp.domain.manager.LocalUserManager
+import kotlinx.coroutines.flow.Flow
 
-class SaveAppEntry (
+class ReadAppEntry(
     private val localUserManager: LocalUserManager
 ) {
-    suspend operator fun invoke(){
-        localUserManager.saveAppEntry()
+    suspend operator fun invoke(): Flow<Boolean> {
+        return localUserManager.readAppEntry()
     }
 }
