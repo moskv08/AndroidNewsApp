04b7e129b3a0b4102a0541cf90fe4273397e970b
moskv08 <fabrizio_39@hotmail.com>
2024-12-09 21:57:57 +0100

    feat: Create ViewModel and Event class for onboarding

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -11,9 +11,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.loc.newsapp.domain.usecases.AppEntryUseCases
 import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
+import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.loc.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -39,7 +41,11 @@ class MainActivity : ComponentActivity() {
                 Box(modifier = Modifier
                     .background(color = MaterialTheme.colorScheme.background)
                 ) {
-                    OnBoardingScreen()
+                    val viewModel: OnBoardingViewModel = hiltViewModel()
+                    OnBoardingScreen(
+                        // viewModel::onEvent is equal to viewModel.event(it)
+                        event = viewModel::onEvent
+                    )
                 }
             }
         }
