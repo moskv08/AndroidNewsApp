201d767efaf7695e78d9bed1a0a4125cb4f14e1d
moskv08 <moskv08@proton.me>
2024-12-18 15:23:35 +0100

    feat: Create interface,dto and domain model for news api

---
+++ app/src/main/java/com/loc/newsapp/data/remote/NewsApi.kt
@@ -0,0 +1,16 @@
+package com.loc.newsapp.data.remote
+
+import com.loc.newsapp.data.remote.dto.NewsResponse
+import com.loc.newsapp.presentation.onboarding.Page
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("everything")
+    suspend fun getNews(
+        @Query("page") page: Int,
+        @Query("sources") sources: String,
+        @Query("apiKey") apiKey: String
+    ): NewsResponse
+}
