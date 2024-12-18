f63942822421a88574e65f6b5e779e4cc8dcca1e
moskv08 <moskv08@proton.me>
2024-12-18 18:15:09 +0100

    feat: Implement paging to receive news from news api

--- app/src/main/java/com/loc/newsapp/data/remote/NewsApi.kt
+++ app/src/main/java/com/loc/newsapp/data/remote/NewsApi.kt
@@ -1,7 +1,7 @@
 package com.loc.newsapp.data.remote
 
 import com.loc.newsapp.data.remote.dto.NewsResponse
-import com.loc.newsapp.presentation.onboarding.Page
+import com.loc.newsapp.util.Constants.API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,6 +11,6 @@ interface NewsApi {
     suspend fun getNews(
         @Query("page") page: Int,
         @Query("sources") sources: String,
-        @Query("apiKey") apiKey: String
+        @Query("apiKey") apiKey: String = API_KEY
     ): NewsResponse
 }
--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -1,7 +1,12 @@
 package com.loc.newsapp.util
 
+import com.loc.newsapp.BuildConfig
+
 object Constants {
 
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
+    const val API_KEY = BuildConfig.API_KEY
+
+
 }
