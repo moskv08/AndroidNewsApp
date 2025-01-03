49ae2fe2820b9524d851d1edd15b3fed3d481e64
moskv08 <fabrizio_39@hotmail.com>
2025-01-03 12:49:09 +0100

    feat: Implement Search Screen and view model logic

--- app/src/main/java/com/loc/newsapp/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/loc/newsapp/presentation/search/SearchViewModel.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(
+class SearchViewModel @Inject constructor(
     private val newsUseCases: NewsUseCases
 ) : ViewModel() {
 
