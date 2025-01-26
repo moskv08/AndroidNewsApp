7417e0b851ce70e067519c0b319a0719cf13e0e5
moskv08 <fabrizio_39@hotmail.com>
2025-01-26 14:23:34 +0100

    fix: Resolve dependeny in import

--- app/src/main/java/com/loc/newsapp/domain/usecases/news/NewsUseCases.kt
+++ app/src/main/java/com/loc/newsapp/domain/usecases/news/NewsUseCases.kt
@@ -1,6 +1,7 @@
 package com.loc.newsapp.domain.usecases.news
 
 import com.loc.newsapp.domain.usecases.news.articles.DeleteArticle
+import com.loc.newsapp.domain.usecases.news.articles.SelectArticle
 import com.loc.newsapp.domain.usecases.news.articles.SelectArticles
 import com.loc.newsapp.domain.usecases.news.articles.UpsertArticle
 
