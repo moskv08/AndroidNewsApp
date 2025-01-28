8b8e587745481649c92c1a1b05da6cc3ee13d489
moskv08 <fabrizio_39@hotmail.com>
2025-01-28 18:35:33 +0100

    fix: Make author nullable

--- app/src/main/java/com/loc/newsapp/domain/model/Article.kt
+++ app/src/main/java/com/loc/newsapp/domain/model/Article.kt
@@ -3,13 +3,12 @@ package com.loc.newsapp.domain.model
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parceler
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @Entity
 data class Article(
-    val author: String,
+    val author: String?,
     val content: String,
     val description: String,
     val publishedAt: String,
