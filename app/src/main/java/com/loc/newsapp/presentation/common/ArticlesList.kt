c6790b93d863e96729dae8807cdc212b210d229b
moskv08 <fabrizio_39@hotmail.com>
2025-01-30 19:06:20 +0100

    fix: Show emptyscreen when bookmark list is empty

--- app/src/main/java/com/loc/newsapp/presentation/common/ArticlesList.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/ArticlesList.kt
@@ -21,7 +21,9 @@ fun ArticlesList(
     articles: List<Article>,
     onClick:(Article) -> Unit
 ) {
-
+    if(articles.isEmpty()) {
+        EmptyScreen()
+    }
     LazyColumn(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(MediumPadding1),
@@ -76,7 +78,6 @@ fun handlePagingResult(articles: LazyPagingItems<Article>): Boolean {
         }
 
         error != null -> {
-            Log.d("WTF", error.error.toString())
             EmptyScreen(error = error)
             false
         }
