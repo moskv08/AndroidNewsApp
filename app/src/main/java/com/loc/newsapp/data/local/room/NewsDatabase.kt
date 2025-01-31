91d1ca8f4c5150c6d22f3266df30ba86700844ce
moskv08 <fabrizio_39@hotmail.com>
2025-01-31 15:43:13 +0100

    fix: Reset database version and exportSchema=false

--- app/src/main/java/com/loc/newsapp/data/local/room/NewsDatabase.kt
+++ app/src/main/java/com/loc/newsapp/data/local/room/NewsDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.loc.newsapp.domain.model.Article
 
-@Database(entities = [Article::class], version = 3)
+@Database(entities = [Article::class], version = 1, exportSchema = false)
 @TypeConverters(NewsTypeConverter::class)
 abstract class NewsDatabase: RoomDatabase() {
 
